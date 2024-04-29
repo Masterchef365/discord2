f81e3262aad2c3eebea8a39657754edc615f97d8
Masterchef365 <duncan.freeman1@gmail.com>
2024-04-29 03:38:24 -0700

    DB interaction!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 import aiosqlite
-from quart import Quart, render_template, websocket, g
+from quart import Quart, render_template, websocket, g, request
 
 app = Quart(__name__)
 
@@ -14,7 +14,6 @@ async def setup_db(db):
     id INTEGER PRIMARY KEY,
     name TEXT
 )''')
-    await db.commit()
 
     await db.execute(
 '''CREATE TABLE IF NOT EXISTS messages (
@@ -27,12 +26,17 @@ async def setup_db(db):
 )''')
     await db.commit()
 
-    print("Setup DB")
+
+# #################### Database operations ####################
+async def get_rooms():
+    db = await get_db()
+    async with db.execute('SELECT name, id FROM rooms') as cursor:
+        return [row async for row in cursor]
 
 
 # #################### Database in g object ####################
 
-async def get_db():
+async def get_db() -> aiosqlite.Connection:
     """Get the database connection"""
     db = getattr(g, '_database', None)
     if db is None:
@@ -53,16 +57,22 @@ async def close_connection(exception):
 @app.route("/")
 async def index():
     """Main page!"""
-    await get_db()
-    return await render_template("index.html")
+    rooms = await get_rooms()
+
+    links = [{'name': name, 'url': f'/rooms/{id_}'} for (name, id_, ) in rooms]
+
+    return await render_template('index.html', links=links)
 
 
 @app.post("/create_room")
 async def create_room():
     """Creates a new room"""
-    db = get_db()
-    cur = db.cursor()
-    await cur.close()
+    data = await request.get_data()
+    db = await get_db()
+
+    _, new_room_name = data.decode('ascii').split('=')
+    await db.execute("INSERT INTO rooms (name) VALUES (?)", (new_room_name,))
+    await db.commit()
 
     return await index()
 
