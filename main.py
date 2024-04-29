a8ae06dd12dc9f29c33dc605451a5492da1ac13b
Masterchef365 <duncan.freeman1@gmail.com>
2024-04-29 01:58:33 -0700

    Start figuring out the database

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from quart import Quart, render_template, websocket
+import sqlite3
+import aiosqlite
+from quart import Quart, render_template, websocket, g
 
 app = Quart(__name__)
 
+DATABASE = 'chat.db'
+
+async def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = await aiosqlite.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+async def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        await db.close()
+
+
 @app.route("/")
-async def hello():
+async def index():
+    await get_db()
     return await render_template("index.html")
+
+
+@app.post("/create_room")
+async def create_room():
+    print("Create room")
+    return await index()
 
 
 if __name__ == "__main__":
